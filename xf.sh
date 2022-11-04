a959a6e30bd033604309ac6d8e4c39c28c213b55
wuidbh23 <94688560+wuidbh23@users.noreply.github.com>
2022-11-04 13:00:02 +0800

    Add files via upload

---
+++ xf.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+##
+
+# Set ARG
+UUID=75389e4d-89a9-4277-aa1f-e004d8ce6c19
+#ARCH="64"
+cat <<EOF >/etc/v2ray/config.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+	"inbounds": [
+		{
+			"listen": "0.0.0.0",
+			"port": 8080,
+			"protocol": "vless",
+			"settings": {
+				"clients": [
+					{
+						"id": "$UUID"
+					}
+				],
+			"decryption": "none"
+		},
+		"streamSettings": {
+			"network": "ws",
+			"wsSettings": {
+					"path": "/$UUID-vless"
+				}
+			}
+		}
+	],
+	"outbounds": [
+		{
+			"protocol": "freedom"
+		}
+	]
+}
+EOF
+
+
+chmod +x /usr/bin/v2ray/v2ray
+ls -la /usr/bin/v2ray/*
+echo "Install done"
+echo "--------------------------------"
+echo "Fly App Name: ${FLY_APP_NAME}"
+echo "Fly App Region: ${FLY_REGION}"
+echo "V2Ray UUID: ${UUID}"
+echo "--------------------------------"
+
+# Run v2ray
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
